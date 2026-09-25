6db8ae324c91b0d249eba637329a42d0f7fa6cf8
Roshan Birla <129684288+roshanbirla07@users.noreply.github.com>
2026-09-25 17:14:45 +0530

    config: remove default export fallback

--- backend/config/variables.js
+++ backend/config/variables.js
@@ -1,29 +1,25 @@
 let config = {};
 
 try {
-  const localConfig = await import("./local_config.js");
-  config = { ...config, ...(localConfig.default || localConfig) };
+  config = { ...config, ...(await import("./local_config.js")) };
 } catch (error) {
   if (error.code !== "ERR_MODULE_NOT_FOUND") throw error;
 }
 
 try {
-  const devConfig = await import("./dev_config.js");
-  config = { ...config, ...(devConfig.default || devConfig) };
+  config = { ...config, ...(await import("./dev_config.js")) };
 } catch (error) {
   if (error.code !== "ERR_MODULE_NOT_FOUND") throw error;
 }
 
 try {
-  const stageConfig = await import("./stage_config.js");
-  config = { ...config, ...(stageConfig.default || stageConfig) };
+  config = { ...config, ...(await import("./stage_config.js")) };
 } catch (error) {
   if (error.code !== "ERR_MODULE_NOT_FOUND") throw error;
 }
 
 try {
-  const prodConfig = await import("./prod_config.js");
-  config = { ...config, ...(prodConfig.default || prodConfig) };
+  config = { ...config, ...(await import("./prod_config.js")) };
 } catch (error) {
   if (error.code !== "ERR_MODULE_NOT_FOUND") throw error;
 }
