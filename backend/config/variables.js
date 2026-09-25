dfb74e94869773048f7ec0035e9fffb80c34b310
Roshan Birla <129684288+roshanbirla07@users.noreply.github.com>
2026-09-25 17:16:31 +0530

    config: select highest-priority config first

--- backend/config/variables.js
+++ backend/config/variables.js
@@ -1,27 +1,27 @@
 let config = {};
 
 try {
-  config = { ...config, ...(await import("./local_config.js")) };
+  config = { ...(await import("./prod_config.js")) };
 } catch (error) {
   if (error.code !== "ERR_MODULE_NOT_FOUND") throw error;
-}
 
-try {
-  config = { ...config, ...(await import("./dev_config.js")) };
-} catch (error) {
-  if (error.code !== "ERR_MODULE_NOT_FOUND") throw error;
-}
+  try {
+    config = { ...(await import("./stage_config.js")) };
+  } catch (error) {
+    if (error.code !== "ERR_MODULE_NOT_FOUND") throw error;
 
-try {
-  config = { ...config, ...(await import("./stage_config.js")) };
-} catch (error) {
-  if (error.code !== "ERR_MODULE_NOT_FOUND") throw error;
-}
+    try {
+      config = { ...(await import("./dev_config.js")) };
+    } catch (error) {
+      if (error.code !== "ERR_MODULE_NOT_FOUND") throw error;
 
-try {
-  config = { ...config, ...(await import("./prod_config.js")) };
-} catch (error) {
-  if (error.code !== "ERR_MODULE_NOT_FOUND") throw error;
+      try {
+        config = { ...(await import("./local_config.js")) };
+      } catch (error) {
+        if (error.code !== "ERR_MODULE_NOT_FOUND") throw error;
+      }
+    }
+  }
 }
 
 export default config;
