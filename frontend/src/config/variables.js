760298603481c20b319ac869e4855d8d0b0ace52
Roshan Birla <129684288+roshanbirla07@users.noreply.github.com>
2026-09-26 00:48:33 +0530

    config: select frontend config directly in variables

--- frontend/src/config/variables.js
+++ frontend/src/config/variables.js
@@ -1,3 +1,19 @@
-import * as config from "./active_config";
+const configs = require.context(".", false, /_config\.js$/);
+
+const priority = [
+  "./prod_config.js",
+  "./stage_config.js",
+  "./dev_config.js",
+  "./local_config.js",
+];
+
+let config = {};
+
+for (const file of priority) {
+  if (configs.keys().includes(file)) {
+    config = configs(file);
+    break;
+  }
+}
 
 export default config;
